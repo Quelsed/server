b4849d9cf1508953e571059eac4ed1fb4f73afd3
Quelsed <120493684+Quelsed@users.noreply.github.com>
2025-06-02 23:58:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -57,4 +57,4 @@ def synthesize():
         return {"error": str(e)}, 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002, debug=True)
+    app.run(host='0.0.0.0', port=5002, ssl_context=('cert.pem', 'key.pem'))
